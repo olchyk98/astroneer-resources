e8c1eac10c3e52049501043b4a76bdecd0f0e68d
Oles Odynets <olchyk98@gmail.com>
2024-11-16 20:25:04 +0100

    feat(types): move article types
    This commit moves the article types from scrapper
    package to types package. This done to simplify
    the use of types between workspace packages.

---
+++ types/article.ts
@@ -0,0 +1,23 @@
+export interface Article {
+  key: ArticleKey
+  name: string
+  iconURL: string
+  type: string
+  imageURL: string
+  planets?: string[] | true
+  unlockCost?: number
+  tier?: string
+  recipe?: ArticleRecipe
+}
+
+export type ArticleKey = string
+
+export interface ArticleRecipe {
+  craftedAt: ArticleKey
+  ingredients: ArticleRecipeIngradient[]
+}
+
+export interface ArticleRecipeIngradient {
+  key: ArticleKey
+  amount: number
+}
--- types/index.ts
+++ types/index.ts
@@ -1,4 +1,1 @@
-export * from './lib/submission'
-export * from './lib/user-score'
-export * from './lib/user'
-export * from './lib/evaluation'
+export * from './article'
