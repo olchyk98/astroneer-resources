6a376f52ea69776761d312af220ec1345c126341
Oles Odynets <olchyk98@gmail.com>
2024-11-17 23:16:16 +0100

    chore(web): enable standalone output
    This commit enables standalone output
    for building the next.js application. This
    enables the output size module to be smaller,
    as it contains all the minimal things
    required to run the server.

--- web/next.config.js
+++ web/next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  transpilePackages: ['@astroneer/scrapper']
+  transpilePackages: ['@astroneer/scrapper'],
+  output: "standalone"
 }
 
 module.exports = nextConfig
