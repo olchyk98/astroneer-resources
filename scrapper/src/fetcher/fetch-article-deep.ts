16a817ff93a0836716a152d7c03169939f27c4e8
Oles Odynets <olchyk98@gmail.com>
2024-11-17 17:29:03 +0100

    feat(scrapper): implement super deep fetching
    This commit extends fetchArticleDeep
    function to output FULL tree of all ingradients. The part
    in question is the "craftedAt" property. The function was
    modded to include recipes for "craftedAt" machines as well.
    
    This made the entire graph in fully expanded view for RTG
    recipe to exceed any viewport limitations. The amount
    of nodes for the RTG recipe was 152.

--- scrapper/src/fetcher/fetch-article-deep.ts
+++ scrapper/src/fetcher/fetch-article-deep.ts
@@ -1,26 +1,42 @@
 import { fetchArticle } from './fetch-article'
-import { ArticleNode } from '@astroneer/types'
+import { Article, ArticleKey, ArticleNode } from '@astroneer/types'
 import { urlComposer } from '../url-composer'
 import { GenericFetchOpts } from './types'
+import { pluck } from 'ramda'
+
+// XXX: Base ingradients/craftedAt machines that will be
+// parsed, and should not be part of graph.
+const unwantedIngredients: Set<ArticleKey> = new Set([
+  'Backpack',
+])
+
+function getIngredientKeysForArticle (article: Article): string[] {
+  const { recipe } = article
+  if (recipe == null) return []
+  const inputIngredients = pluck('key', recipe.ingredients)
+  const keys = [ ...inputIngredients, recipe.craftedAt ]
+  return keys.filter((l) => !!l && !unwantedIngredients.has(l))
+}
 
 /**
  * This function will invoke fetchArticle
  * for the input URL and every single recipe
- * ingradient recursively. At the end
- * a tree will be returned containing detailed
- * information about each step of the production
+ * ingredient recursively, including recipes for
+ * "createdAt" machines.
+ * At the end a tree will be returned containing
+ * detailed information about each step of the production
  * process in form of ArticleNode structure.
  * */
 export async function fetchArticleDeep (url: string, opts?: GenericFetchOpts): Promise<ArticleNode> {
   const article = await fetchArticle(url, opts)
   const node: ArticleNode = { article }
   if (article.recipe != null) {
-    const { ingredients } = article.recipe
-    if (ingredients.length > 0) {
+    const ingredientKeys = getIngredientKeysForArticle(article)
+    if (ingredientKeys.length > 0) {
       node.children = []
     }
-    for await (const i of ingredients) {
-      const childNode = await fetchArticleDeep(urlComposer(i.key), opts)
+    for await (const key of ingredientKeys) {
+      const childNode = await fetchArticleDeep(urlComposer(key), opts)
       node.children!.push(childNode)
     }
   }
