6d953c579ba2ccf1cbbf7f2c30736d0eb04ebc71
Oles Odynets <olchyk98@gmail.com>
2024-11-24 13:39:26 +0100

    feat(utils): include planets as children
    This commit updates the get-child-ref-keys-for-article
    function in order to include planets as children. This
    allows to construct a more detailed recipe/usages overview
    for articles.

--- utils/lib/article/get-child-ref-keys-for-article.ts
+++ utils/lib/article/get-child-ref-keys-for-article.ts
@@ -1,5 +1,15 @@
 import { pluck } from 'ramda'
 import { Article, ArticleKey } from '@astroneer/types'
+import { planetKeys } from '../planets'
+import { isStringArray } from '../misc'
+
+function getPlanetKeys (planets: Article['planets']): string[] {
+  if (planets == null || planets === false) return []
+  if (planets === true) return planetKeys
+  if (planets.length === 0) return []
+  if (isStringArray(planets)) return planets
+  return pluck('key', planets)
+}
 
 /**
  * The function consumes an article and outputs
@@ -8,12 +18,15 @@ import { Article, ArticleKey } from '@astroneer/types'
  * This includes ingredient keys and craftedAt
  * module key.
  * */
-export function getChildRefKeysForArticle (article: Pick<Article, 'key' | 'recipe'>): ArticleKey[] {
-  const { recipe } = article
+export function getChildRefKeysForArticle (article: Pick<Article, 'key' | 'recipe' | 'planets'>): ArticleKey[] {
+  const { recipe, planets } = article
   const keys: ArticleKey[] = []
   if (recipe != null) {
     keys.push(...pluck('key', article.recipe?.ingredients ?? []))
     keys.push(recipe.craftedAt)
+  }
+  if (planets != null) {
+    keys.push(...getPlanetKeys(planets))
   }
   return keys
 }
